d554e5d26a326606fb8234229b1400f0188fa083
haejun <haejun.chung210@gmail.com>
2020-06-11 00:46:48 -0400

    Merged sorted array

--- ae_insertion_sort.js
+++ ae_insertion_sort.js
@@ -1,3 +1,24 @@
+function insertionSort(array){ //[1,3,4]
+
+    for(let i = 1; i < array.length; i++){  
+        let candidatePointer = i; 
+
+        while(array[candidatePointer] < array[candidatePointer - 1] && candidatePointer - 1 >= 0){ 
+            swap(array, candidatePointer, candidatePointer - 1); 
+            candidatePointer--; 
+        }
+    }
+
+    return array
+}
+
+function swap(array, index1, index2){
+    [array[index1], array[index2]] = [array[index2], array[index1]]; 
+}
+
+
+
+
 function insertionSort(array) {
     for (let i = 1; i < array.length; i++) {
         let j = i;
