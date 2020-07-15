fda0e9a582d6fd97dce8f735e9129174d727b857
haejun <haejun.chung210@gmail.com>
2020-07-14 23:02:02 -0400

    Has cycle

---
+++ lc_has_cycle.js
@@ -0,0 +1,18 @@
+// Given a linked list, determine if it has a cycle in it.
+
+// To represent a cycle in the given linked list, we use an integer pos which 
+// represents the position(0 - indexed) in the linked list where tail connects to.
+// If pos is - 1, then there is no cycle in the linked list.
+
+var hasCycle = function (head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) return true;
+    }
+
+    return false;
+};
