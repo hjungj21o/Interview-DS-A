036f3ac2ea4cdb1ace6bc6dcde78b6a00245c2c1
haejun <haejun.chung210@gmail.com>
2020-08-20 20:34:24 -0400

    1528 Shuffle string

---
+++ lc_1528_shuffle_string.js
@@ -0,0 +1,17 @@
+// Given a string s and an integer array indices of the same length.
+
+// The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+// Return the shuffled string.
+
+var restoreString = function (s, indices) {
+    let result = new Array(indices.length)
+    // let realIdx;
+    for (i in s) {
+        let realIdx = indices[i];
+        result[realIdx] = s[i]
+    }
+
+    return result.join("")
+
+};
