1119873ff919fb9ba47c3bb9d1d3c30e768e7925
haejun <haejun.chung210@gmail.com>
2020-08-20 21:08:50 -0400

    328 Odd even linked list

---
+++ lc_328_odd_even_linked_list.js
@@ -0,0 +1,33 @@
+// Given a singly linked list, group all odd nodes together followed by the even nodes.Please note here we are talking about the node number and not the value in the nodes.
+
+// You should try to do it in place.The program should run in O(1) space complexity and O(nodes) time complexity.
+
+//     Example 1:
+
+// Input: 1 -> 2 -> 3 -> 4 -> 5 -> NULL
+// Output: 1 -> 3 -> 5 -> 2 -> 4 -> NULL
+// Example 2:
+
+// Input: 2 -> 1 -> 3 -> 5 -> 6 -> 4 -> 7 -> NULL
+// Output: 2 -> 3 -> 6 -> 7 -> 1 -> 5 -> 4 -> NULL
+
+var oddEvenList = function (head) {
+    if (!head) return null;
+    let even = head.next;
+    let odd = head;
+
+    let evenHead = even;
+
+    while (even && even.next) {
+        odd.next = even.next;
+        odd = odd.next;
+
+        even.next = odd.next;
+        even = even.next;
+
+    }
+
+    odd.next = evenHead;
+
+    return head;
+};
