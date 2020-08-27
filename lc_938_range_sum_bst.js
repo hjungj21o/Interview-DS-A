2c9c91289fe894c8a23069084075336471283492
haejun <haejun.chung210@gmail.com>
2020-08-26 23:09:34 -0400

    1295 find nums with even num digits

--- lc_938_range_sum_bst.js
+++ lc_938_range_sum_bst.js
@@ -1,18 +1,18 @@
-Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R(inclusive).
+// Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R(inclusive).
 
-The binary search tree is guaranteed to have unique values.
+// The binary search tree is guaranteed to have unique values.
 
 
 
-Example 1:
+// Example 1:
 
-Input: root = [10, 5, 15, 3, 7, null, 18], L = 7, R = 15
-Output: 32
+// Input: root = [10, 5, 15, 3, 7, null, 18], L = 7, R = 15
+// Output: 32
 
-Example 2:
+// Example 2:
 
-Input: root = [10, 5, 15, 3, 7, 13, 18, 1, null, 6], L = 6, R = 10
-Output: 23
+// Input: root = [10, 5, 15, 3, 7, 13, 18, 1, null, 6], L = 6, R = 10
+// Output: 23
 
 var rangeSumBST = function (root, L, R) {
     if (!root) return 0;
