e942758d1ca9794ccaef7ee59c5df569be628f4e
haejun <haejun.chung210@gmail.com>
2020-08-28 00:12:15 -0400

    283 move zeroes

--- lc_283_move_zeros.js
+++ lc_283_move_zeros.js
@@ -8,3 +8,16 @@
 
 // You must do this in -place without making a copy of the array.
 // Minimize the total number of operations.
+
+var moveZeroes = function (nums) {
+    let zeroP = 0;
+
+    for (let norP = 0; norP < nums.length; norP++) {
+        if (nums[norP] !== 0) {
+            [nums[zeroP], nums[norP]] = [nums[norP], nums[zeroP]];
+            zeroP++;
+        }
+    }
+
+
+};
