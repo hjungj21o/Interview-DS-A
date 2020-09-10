eb4885cdf6d70a3bf7953c41af976cdb4901b135
haejun <haejun.chung210@gmail.com>
2020-09-10 18:52:35 -0400

    Python 1480 running sum of array

---
+++ lc_1480_running_sum_of_arr.py
@@ -0,0 +1,18 @@
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+# Return the running sum of nums.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,4]
+# Output: [1,3,6,10]
+# Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        result = [nums[0]]
+        for i in range(1, len(nums)):
+            result.append(result[-1] + nums[i])
+        return result
